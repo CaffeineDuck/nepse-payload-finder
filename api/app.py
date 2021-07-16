9f1cef62561e5c0f9747843fad0d533355db48af
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-16 19:21:06 +0545

    [update] Chromedriver path and Chrome binary are fetched from environment variables

--- api/app.py
+++ api/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
+
+from api.config import CHROME_BINARY_PATH, CHROME_DRIVER_PATH
+
 from .generator import PayloadGenerator
 
-driver = "driver/chromedriver"
 web_url = "https://newweb.nepalstock.com/company/detail/2917"
 api_url = "https://newweb.nepalstock.com/api/nots/security/2917"
 
-generator = PayloadGenerator(driver, web_url, api_url)
+generator = PayloadGenerator(CHROME_BINARY_PATH, CHROME_DRIVER_PATH, web_url, api_url)
 app = FastAPI()
 
 
@@ -14,7 +16,8 @@ async def root():
     payload_id = generator.get_payload_id()
     return {"payload_id": payload_id}
 
-@app.get('/fetch')
+
+@app.get("/fetch")
 async def fetch():
     payload_id = generator.fetch_payload_id()
     return {"payload_id": payload_id}
