df02418df22ab9e07394ef0ef25c496f2a56e99e
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-16 19:22:15 +0545

    [update] Generator accepts chrome binary as well instead of default

--- api/generator.py
+++ api/generator.py
@@ -1,21 +1,28 @@
+import re
 import threading
 import time
+
+from cachetools import TTLCache
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import re
-from cachetools import TTLCache
 
 
 class PayloadGenerator:
-    def __init__(self, webdriver_path: str, web_url: str, api_url: str) -> None:
+    def __init__(
+        self, binary_path: str, webdriver_path: str, web_url: str, api_url: str
+    ) -> None:
         caps = DesiredCapabilities.CHROME
         caps["goog:loggingPrefs"] = {"performance": "ALL"}
 
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
+        options._binary_location = binary_path
 
         self.driver = webdriver.Chrome(
-            webdriver_path, desired_capabilities=caps, options=options
+            webdriver_path,
+            keep_alive=False,
+            desired_capabilities=caps,
+            options=options,
         )
 
         # Regex for filtering data
@@ -27,7 +34,7 @@ class PayloadGenerator:
         self.web_url = web_url
 
         # Cache
-        self.payload_id_cache = TTLCache(1, 200) 
+        self.payload_id_cache = TTLCache(1, 200)
 
         # Background Tasks
         thread = threading.Thread(target=self._background_updater, args=())
@@ -36,12 +43,13 @@ class PayloadGenerator:
 
     def _background_updater(self) -> None:
         while True:
-           self.fetch_payload_id() 
-           time.sleep(60)
-
+            self.fetch_payload_id()
+            time.sleep(60)
 
     def get_logs(self) -> str:
         self.driver.get(self.web_url)
+        time.sleep(5)
+
         logs = self.driver.get_log("performance")
 
         return ([log for log in logs if self.api_url in log.get("message")])[0].get(
@@ -53,23 +61,13 @@ class PayloadGenerator:
         match = self.payload_id_regex.findall(log)[0]
         payload_id = int(self.number_regex.findall(match)[0])
 
-        self.payload_id_cache['payload_id'] = payload_id
+        self.payload_id_cache["payload_id"] = payload_id
         return payload_id
 
     def get_payload_id(self) -> int:
-        payload_id = self.payload_id_cache.get('payload_id')
-        
+        payload_id = self.payload_id_cache.get("payload_id")
+
         if not payload_id:
             payload_id = self.fetch_payload_id()
 
         return payload_id
-
-
-if __name__ == "__main__":
-    generator = PayloadGenerator(
-        "driver/chromedriver",
-        "https://newweb.nepalstock.com/company/detail/2917",
-        "https://newweb.nepalstock.com/api/nots/security/2917",
-    )
-    payload_id = generator.get_payload_id()
-    print(payload_id)
